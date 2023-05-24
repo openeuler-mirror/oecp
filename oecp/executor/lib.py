d7fe212f28bb404e26c1ff40cc154a70463ea330
yang-yulong-007 <528553518@qq.com>
2023-05-24 11:36:22 +0800

    修改代码规范问题

--- oecp/executor/lib.py
+++ oecp/executor/lib.py
@@ -30,16 +30,16 @@ class LibCompareExecutor(CompareExecutor):
         self.dump_other = dump_other.run()
         self.data = 'data'
 
-    def _compare_result(self, dump_base, dump_other, single_result=CMP_RESULT_SAME):
+    def compare_result(self, dump_base, dump_other, single_result=CMP_RESULT_SAME):
         count_result = {'same': 0, 'more': 0, 'less': 0, 'diff': 0}
         category = dump_base['category']
+        cmp_type = self.config.get('compare_type')
         result = CompareResultComposite(CMP_TYPE_RPM, single_result, dump_base['rpm'], dump_other['rpm'], category)
         base_libs = dump_base[self.data]
         other_libs = dump_other[self.data]
         flag_vrd = self.extract_version_flag(dump_base['rpm'], dump_other['rpm'])
         if not base_libs and not other_libs:
-            logger.debug(
-                f"No {self.config.get('compare_type')} package found, ignored with {dump_base['rpm']} and {dump_other['rpm']}")
+            logger.debug(f"No {cmp_type} package found, ignored with {dump_base['rpm']} and {dump_other['rpm']}")
             return result
         component_results = self.match_library_pairs(base_libs, other_libs, flag_vrd, CMP_TYPE_RPM_LIB)
         for component_result in component_results:
@@ -47,8 +47,8 @@ class LibCompareExecutor(CompareExecutor):
                 self.count_cmp_result(count_result, sub_component_result[-1])
                 if not self.config.get('show_same', False) and sub_component_result[-1] == CMP_RESULT_SAME:
                     continue
-                data = CompareResultComponent(self.config.get('compare_type'), sub_component_result[-1],
-                                              sub_component_result[0], sub_component_result[1])
+                data = CompareResultComponent(cmp_type, sub_component_result[-1], sub_component_result[0],
+                                              sub_component_result[1])
                 if sub_component_result[-1] not in CMP_SAME_RESULT and single_result == CMP_RESULT_SAME:
                     single_result = CMP_RESULT_DIFF
                     result.set_cmp_result(single_result)
@@ -63,7 +63,7 @@ class LibCompareExecutor(CompareExecutor):
         for single_pair in similar_dumpers:
             if single_pair:
                 dump_base, dump_other = single_pair[0], single_pair[1]
-                result = self._compare_result(dump_base, dump_other)
+                result = self.compare_result(dump_base, dump_other)
                 compare_list.append(result)
         return compare_list
 
